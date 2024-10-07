02059239909b0bc4e725cdb6af7531201fde5fb8
tuanhuym <haclong168h@gmail.com>
2024-10-07 11:06:38 +0700

    Delete orderpage

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,10 +3,10 @@ import { createBrowserRouter } from 'react-router-dom';
 import Homepage from './components/common/Homepage';
 import LoginPage from './components/common/loginpage';
 import RegisterPage from './components/common/registerpage';
-import ServicePage from './components/common/servicepage';
-import OrderPage from './components/common/orderinformationpage';
-import OrderDetailPage from './components/common/orderdetailpage';
-import CheckoutPage from './components/common/checkout';
+// import ServicePage from './components/common/servicepage';
+// import OrderPage from './components/common/orderinformationpage';
+// import OrderDetailPage from './components/common/orderdetailpage';
+// import CheckoutPage from './components/common/checkout';
 import ProfilePage from './components/common/profilepage';
 import OrderHistory from './components/common/orderhistory';
 import TrackOrderPage from './components/common/trackorder';
@@ -25,22 +25,22 @@ export const router = createBrowserRouter([
     path: "register",
     element: <RegisterPage />,
   },
-  {
-    path: "service",
-    element: <ServicePage />,
-  },
-  {
-    path: "orderinformation",
-    element: <OrderPage />,
-  },
-  {
-    path: "orderdetail",
-    element: <OrderDetailPage />,
-  },
-  {
-    path: "checkout",
-    element: <CheckoutPage />,
-  },
+  // {
+  //   path: "service",
+  //   element: <ServicePage />,
+  // },
+  // {
+  //   path: "orderinformation",
+  //   element: <OrderPage />,
+  // },
+  // {
+  //   path: "orderdetail",
+  //   element: <OrderDetailPage />,
+  // },
+  // {
+  //   path: "checkout",
+  //   element: <CheckoutPage />,
+  // },
   {
     path: "profile",
     element: <ProfilePage />,
